329ef5df9e526c885420c96d2ed05df64486d1ed
liamkenny <lck3nny@gmail.com>
2018-05-18 18:09:01 +0100

    Created a more detailed view for selected events

--- app/src/main/java/com/example/liamkenny/unionapp/EventsAdapter.java
+++ app/src/main/java/com/example/liamkenny/unionapp/EventsAdapter.java
@@ -56,7 +56,6 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.ViewHolder
         mEventNames = eventNames;
         mEventInfo = eventInfo;
         mEventDate = eventDates;
-        //mContext = context;
     }
 
 
