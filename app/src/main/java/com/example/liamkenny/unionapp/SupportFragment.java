21881ee8dfcc8a939654947d180cc8f749be3889
liamkenny <lck3nny@gmail.com>
2018-05-18 14:22:01 +0100

    Tidied union zone fragments to remove useless arg values

--- app/src/main/java/com/example/liamkenny/unionapp/SupportFragment.java
+++ app/src/main/java/com/example/liamkenny/unionapp/SupportFragment.java
@@ -9,23 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SupportFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SupportFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class SupportFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class SupportFragment extends Fragment {
+
+
+
 
     private OnFragmentInteractionListener mListener;
 
@@ -33,32 +21,13 @@ public class SupportFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SupportFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
+
     public static SupportFragment newInstance(String param1, String param2) {
         SupportFragment fragment = new SupportFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -67,17 +36,10 @@ public class SupportFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_support, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-
     }
 
     @Override
@@ -86,16 +48,6 @@ public class SupportFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
