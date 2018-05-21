c186796379bb8c167919ef0d60e93c621bb5a440
Matt <mc00905@surrey.ac.uk>
2018-05-21 11:41:11 +0100

    Method signatures added: started commenting too

--- app/src/main/java/com/example/liamkenny/unionapp/dbHelper.java
+++ app/src/main/java/com/example/liamkenny/unionapp/dbHelper.java
@@ -6,13 +6,32 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class dbHelper extends SQLiteOpenHelper {
 
+    /**
+     *
+     * Helper class holding all of the methods related to the device local SQL database:
+     *      The function of this database is to allow the current contents of the user's basket
+     *      to be stored program wide, without pushing all the data through the app in the intents.
+     *
+     */
 
 
+
+    //Init vars: make more resilient by not hard coding tables names
     private static final int DB_VERSION = 1;
     private static final String database_name = "basket_database";
     private static final String Product_Table_Name = "product";
     private static  dbHelper sInstance;   //instant of this class
     private static SQLiteDatabase.CursorFactory factory = null;
+
+
+    /**
+     * Use of Singleton design pattern to ensure only one read/write request
+     * is made at once
+     *
+     *
+     * @param context
+     * @return
+     */
 
     public static synchronized dbHelper getInstance(Context context) {
 
@@ -44,7 +63,13 @@ public class dbHelper extends SQLiteOpenHelper {
 
     private void createProductTable(){}
 
+    private void addProductToBasket(){};
 
+    private void removeProductFromBasket(){};
+
+    private void updateProductQuantity(){};
+
+    private void clearBasket(){};
 
 }
 
